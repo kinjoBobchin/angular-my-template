09bd36ff36de2e4dc490ac18d06c73fac00e5b7f
{bobchin} <kinjo.bobchin@gmail.com>
2018-06-14 00:49:29 +0900

    sassに変換。特にloaderは必要なし

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  styleUrls: ['./app.component.css']
   templateUrl: './app.component.pug',
+  styleUrls: ['./app.component.sass']
 })
 export class AppComponent {
   title = 'app';
