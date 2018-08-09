11943b442e345e94d729e81b6205a85e1dd18589
bobchin <kinjo.bobchin@gmail.com>
2018-08-10 02:08:06 +0900

    👍typescript対応
    このままではcyが読み込めないので、tsconfigを追加

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,5 +1,27 @@
-const cypressTypeScriptPreprocessor = require('./cy-ts-preprocessor')
+const wp = require('@cypress/webpack-preprocessor');
+
+const webpackOptions = {
+  resolve: {
+    extensions: ['.ts', '.js']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        exclude: [/node_modules/],
+        use: [
+          {
+            loader: 'ts-loader'
+          }
+        ]
+      }
+    ]
+  }
+};
 
 module.exports = on => {
-  on('file:preprocessor', cypressTypeScriptPreprocessor)
-}
+  const options = {
+    webpackOptions
+  };
+  on('file:preprocessor', wp(options));
+};
