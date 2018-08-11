89c7d3027cf3410f3664c4e7c10981e2cfd40160
bobchin <kinjo.bobchin@gmail.com>
2018-08-11 18:15:02 +0900

    👍 ng g component top-toolbar
    トップに表示するツールバーを作成

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TopToolbarComponent } from './top-toolbar/top-toolbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopToolbarComponent
   ],
   imports: [
     BrowserModule,
