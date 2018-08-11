47670b9152c583fb3a78993d33e000768cfa70c7
bobchin <kinjo.bobchin@gmail.com>
2018-08-11 20:38:38 +0900

    👍 top-menuにiconを追加
    mat-iconで追加可能

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -8,7 +9,12 @@ import { TopToolbarComponent } from './top-toolbar/top-toolbar.component';
 
 @NgModule({
   declarations: [AppComponent, TopToolbarComponent],
-  imports: [BrowserModule, BrowserAnimationsModule, MatToolbarModule],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
