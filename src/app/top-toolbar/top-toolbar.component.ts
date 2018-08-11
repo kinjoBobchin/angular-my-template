7fe367082eb63113ab35b1982952c41d6aa178f7
bobchin <kinjo.bobchin@gmail.com>
2018-08-11 19:04:02 +0900

    👍css→sassに変更
    ng componentのコマンドを打つときに--styleext=sassをすると初期設定されている

--- src/app/top-toolbar/top-toolbar.component.ts
+++ src/app/top-toolbar/top-toolbar.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-top-toolbar',
   templateUrl: './top-toolbar.component.pug',
-  styleUrls: ['./top-toolbar.component.css']
+  styleUrls: ['./top-toolbar.component.sass']
 })
 export class TopToolbarComponent implements OnInit {
   constructor() {}
