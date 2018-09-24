0990aacd043b9b33024e31470793a98e1b2b10f3
bobchin <kinjo.bobchin@gmail.com>
2018-09-25 01:30:50 +0900

    ✨ng g component side-nav --styleext=sass
    # 🐛 バグ修正
    # 👍 機能改善
    # ✨ 部分的な機能追加
    # 🎉 盛大に祝うべき大きな機能追加
    # ♻️ リファクタリング
    # 🚿 不要な機能・使われなくなった機能の削除
    # 💚 テストやCIの修正・改善
    # 👕 Lintエラーの修正やコードスタイルの修正
    # 🚀 パフォーマンス改善
    # 🆙 依存パッケージなどのアップデート
    # 🔒 新機能の公開範囲の制限
    # 👮 セキュリティ関連の改善

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,9 +9,10 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { TopToolbarComponent } from './top-toolbar/top-toolbar.component';
 import { FavoriteListsComponent } from './favorite-lists/favorite-lists.component';
+import { SideNavComponent } from './side-nav/side-nav.component';
 
 @NgModule({
-  declarations: [AppComponent, TopToolbarComponent, FavoriteListsComponent],
+  declarations: [AppComponent, TopToolbarComponent, FavoriteListsComponent, SideNavComponent],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
