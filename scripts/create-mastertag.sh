f96037c54914c3f7e92bf65f50441b7a1e67dd98
bobchin <kinjo.bobchin@gmail.com>
2018-09-28 02:51:06 +0900

    🐛$CIRCLE_TAGが出力されるか確認

--- scripts/create-mastertag.sh
+++ scripts/create-mastertag.sh
@@ -3,9 +3,9 @@
 # enviroment variables
 NOW=$(date +'%Y_%m%d_%H%M')
 BRANCH_NAME=${CIRCLE_BRANCH}
+CIRCLE_TAG=${CIRCLE_TAG}
 
 tag_name="${BRANCH_NAME}_${NOW}"
 echo $tag_name
-echo ${CIRCLE_PR_REPONAME}
-echo ${CIRCLE_TAG}
+echo $CIRCLE_TAG
 git tag $tag_name && git push origin --tags
