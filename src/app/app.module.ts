72d59dc51dee2b69299adc31f8e604adc73b69e4
bobchin <kinjo.bobchin@gmail.com>
2018-10-10 16:20:27 +0900

    ♻️フォルダに移動したのでファイルパスを変更

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,9 +11,9 @@ import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { TopToolbarComponent } from './top-toolbar/top-toolbar.component';
-import { FavoriteListsComponent } from './favorite-lists/favorite-lists.component';
-import { SideNavComponent } from './side-nav/side-nav.component';
+import { TopToolbarComponent } from './component/top-toolbar/top-toolbar.component';
+import { FavoriteListsComponent } from './component/favorite-lists/favorite-lists.component';
+import { SideNavComponent } from './component/side-nav/side-nav.component';
 
 @NgModule({
   declarations: [
