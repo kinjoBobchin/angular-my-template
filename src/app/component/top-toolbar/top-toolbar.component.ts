7bc8a543292fb2f12f64c011a971486deda0e994
bobchin <kinjo.bobchin@gmail.com>
2018-10-15 02:52:37 +0900

    ✨入力側の設定①
    ngOnInitで初期値はサービスのisOpenの値をfalseに設定

--- src/app/component/top-toolbar/top-toolbar.component.ts
+++ src/app/component/top-toolbar/top-toolbar.component.ts
@@ -1,12 +1,16 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { SideNavStatusService } from 'src/app/service/side-nav-status/side-nav-status.service';
 @Component({
   selector: 'app-top-toolbar',
   templateUrl: './top-toolbar.component.pug',
   styleUrls: ['./top-toolbar.component.sass']
 })
-export class TopToolbarComponent {
-  menuClickedStatus = false;
+export class TopToolbarComponent implements OnInit {
+  constructor(private sideNavStatusService: SideNavStatusService) {}
+
+  ngOnInit() {
+    this.sideNavStatusService.isOpen = false;
+  }
 
   onMenuClicked() {
     return (this.menuClickedStatus = !this.menuClickedStatus);
