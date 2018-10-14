3f2ba8e7455bc6070c357a67f6446ed3909d1cda
bobchin <kinjo.bobchin@gmail.com>
2018-10-14 18:19:19 +0900

    ✨メニューアイコンをクリックして、ステータスを渡す準備が整った

--- src/app/component/top-toolbar/top-toolbar.component.ts
+++ src/app/component/top-toolbar/top-toolbar.component.ts
@@ -1,12 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-top-toolbar',
   templateUrl: './top-toolbar.component.pug',
   styleUrls: ['./top-toolbar.component.sass']
 })
-export class TopToolbarComponent implements OnInit {
-  constructor() {}
+export class TopToolbarComponent {
+  menuClickedStatus = false;
 
-  ngOnInit() {}
+  onMenuClicked() {
+    return (this.menuClickedStatus = !this.menuClickedStatus);
+  }
 }
