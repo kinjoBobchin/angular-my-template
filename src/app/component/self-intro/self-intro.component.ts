d4096dad4dbe26814c3541c38735930f302bd9f5
Unknown <kinjou_yuya@ca-adv.co.jp>
2018-11-02 17:14:18 +0900

    new: 自己紹介カードを追加

--- src/app/component/self-intro/self-intro.component.ts
+++ src/app/component/self-intro/self-intro.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { introData } from './self-intro.mock';
 
 @Component({
   selector: 'app-self-intro',
@@ -7,7 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SelfIntroComponent implements OnInit {
 
-  constructor() { }
+  constructor() {}
+
+  selfData = introData;
 
   ngOnInit() {
   }
