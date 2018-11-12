ce23820ca37e2a25ba89b69b86d14725c6a8bd25
Unknown <kinjou_yuya@ca-adv.co.jp>
2018-11-12 17:37:11 +0900

    chore: ルールを追加

--- scripts/version.sh
+++ scripts/version.sh
@@ -1,5 +1,27 @@
 #!/bin/bash
 
+# コミットコメントからversionを変更するシェル
+
+# ルール
+# + 1.0.0 (npm version major)
+
+# + 0.1.0
+# feat: A new feature
+# fix: A bug fix
+# refactor: A code change that neither fixes a bug nor adds a feature
+# perf: A code change that improves performance
+# test: Adding missing or correcting existing tests
+# chore: Changes to the build process or auxiliary tools and libraries such as documentation generation
+# new: create a file
+
+# + 0.0.1
+# docs: Documentation only changes
+# style: Changes that do not affect the meaning of the code (white-space, formatting, missing semi-colons, etc)
+# package: update a package
+# delete: delete the file
+
+
+
 CURRENT_PKG_VERSION=`node -pe 'require("./package.json").version'`
 echo $CURRENT_PKG_VERSION
 
