034e8c943f0f56c9b4a5f0fb3ea749568498ce17
金城 裕也 <kinjou_yuya@ca-adv.co.jp>
2018-11-13 16:08:26 +0900

    chore: バージョンタグが一定数以上存在すると、古いタグを削除する

--- scripts/version.sh
+++ scripts/version.sh
@@ -2,10 +2,6 @@
 
 # コミットコメントからversionを変更するシェル
 
-CURRENT_PKG_VERSION=`node -pe 'require("./package.json").version'`
-echo 変更前のバージョン$CURRENT_PKG_VERSION
-
-
 # version変更ルール
 
 ##############################################
@@ -37,13 +33,28 @@ echo 変更前のバージョン$CURRENT_PKG_VERSION
 ##############################################
 # プレフィクスなし
 
+CURRENT_PKG_VERSION=`node -pe 'require("./package.json").version'`
+echo 変更前のバージョン$CURRENT_PKG_VERSION
+
 commit_log=$(git log master..HEAD --oneline | awk '{print $2}') # masterから最新のコミットまでのコミットログを取得し、コメントのみ抽出
 # echo $commit_log
 case $commit_log in
-  *feat* | *refactor* | *perf* | *new* | *delete* | *fix* ) npm version minor --git-tag-version=false;;
-  *fix* | *chore* | *test* | *docs* | *style* | *package* ) npm version patch --git-tag-version=false ;;
+  *feat* | *refactor* | *perf* | *new* | *delete* ) npm version minor ;;
+  *fix* | *chore* | *test* | *docs* | *style* | *package* ) npm version patch ;;
   * ) echo "今回、バージョン変更はなしです";;
 esac
 
 CURRENT_PKG_VERSION=`node -pe 'require("./package.json").version'`
 echo 変更後のバージョン$CURRENT_PKG_VERSION
+
+# タグが10個以上生成されたら、古いタグを削除
+PKG_VERSIONS=$(git tag -l [0-9]*.[0-9]*.[0-9]*) # 1.0.0, 1.12.2, 12.0.12 ・・・
+PKG_VERSIONS_ARRAY=($PKG_VERSIONS)              # 個数を計算するために、配列に入れる
+PKG_VERSIONS_COUNT=${#PKG_VERSIONS_ARRAY[@]}    # 配列の個数をカウント
+
+if [ $PKG_VERSIONS_COUNT -gt 10 ]; then
+  OLDEST_PKG_VERSIONS_TAG=$(git tag -l [0-9]*.[0-9]*.[0-9]* | sort -n | head -n 1) # 一番古いタグを取り出す
+  git tag -d "${OLDEST_PKG_VERSIONS_TAG}";                                              # 一番古いタグをローカルで削除
+  git push --delete origin "${OLDEST_PKG_VERSIONS_TAG}";                                # 一番古いタグをリモートでも削除
+  echo "一番古い${OLDEST_PKG_VERSIONS_TAG}を削除しました"
+fi
